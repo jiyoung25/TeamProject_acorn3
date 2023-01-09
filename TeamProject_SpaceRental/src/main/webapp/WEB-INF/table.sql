8241eb25ac93264aa7fc2b8d4aceccd540b43135
leejiyoung <wldud5117@naver.com>
2023-01-09 17:41:31 +0900

    chatbot table DB정보 추가

--- TeamProject_SpaceRental/src/main/webapp/WEB-INF/table.sql
+++ TeamProject_SpaceRental/src/main/webapp/WEB-INF/table.sql
@@ -6,3 +6,18 @@ CREATE TABLE space_users(
    profile VARCHAR2(100),
    regdate DATE
 );
+
+CREATE TABLE chatbot(
+    num NUMBER PRIMARY KEY,
+    category VARCHAR2(100) NOT NULL,
+    question VARCHAR2(500) NOT NULL,
+    answer VARCHAR2(500) NOT NULL
+)
+
+CREATE SEQUENCE chatbot_seq
+-- sample data
+-- INSERT INTO CHATBOT
+-- (num, category, question, answer)
+-- VALUES(chatbot_seq.NEXTVAL, '예약', '예약은 어떻게 하나요?', '이렇게 예약하시면 됩니다.')
+-- VALUES(chatbot_seq.NEXTVAL, '예약', '일정을 변경하고 싶어요.', '판매자와 상의하세요.')
+-- VALUES(chatbot_seq.NEXTVAL, '예약', '예약했는데 확정이 안나요..', '판매자에게 연락하세요.')
