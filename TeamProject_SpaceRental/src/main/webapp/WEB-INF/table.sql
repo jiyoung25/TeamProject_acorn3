9f8fce26e5de3936c0d63029b15ed539d23380e0
WOO BIN LEE <iopplm89@gmail.com>
2023-01-10 18:32:48 +0900

    구매자 table 추가함

--- TeamProject_SpaceRental/src/main/webapp/WEB-INF/table.sql
+++ TeamProject_SpaceRental/src/main/webapp/WEB-INF/table.sql
@@ -2,9 +2,40 @@ CREATE TABLE space_users(
    code NUMBER NOT NULL,
    id VARCHAR2(100) PRIMARY KEY,
    pwd VARCHAR2(100) NOT NULL,
-   email VARCHAR2(100),
-   profile VARCHAR2(100),
+   email VARCHAR2(100) NOT NULL,
+   profile VARCHAR2(100) NOT NULL,
    regdate DATE
+);
+
+CREATE TABLE reserv_list(
+	users_num NUMBER PRIMARY KEY,
+	space_num NUMBER NOT NULL,
+	seller_num NOT NULL,
+	reserv_num NUMBER NOT NULL,
+	reserv_date VARHAR2(100) NOT NULL
+);
+
+CREATE TABLE dibson_list(
+	user_num NUMBER PRIMARY KEY,
+	dibson_num NUMBER NOT NULL,
+	space_num NUMBER NOT NULL
+);
+
+CREATE TABLE review(
+	review_num NUMBER PRIMARY KEY,
+	users_num NUMBER PRIMARY KEY,
+	space_num NUMBER NOT NULL,
+	review_title NOT NULL,
+	review_content NOT NULL,
+	review_rating NOT NULL
+);
+
+CREATE TABLE qna(
+	qna_num NUMBER PRIMARY KEY,
+	users_num NUMBER PRIMARY KEY,
+	space_num NUMBER NOT NULL,
+	qna_title VARCHAR2(100) NOT NULL,
+	qna_content VARCHAR2(100) NOT NULL
 );
 
 CREATE TABLE chatbot(
