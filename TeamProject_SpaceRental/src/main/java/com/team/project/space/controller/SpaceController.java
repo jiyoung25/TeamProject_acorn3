83ae3aeed1c8c868b647adb7627719b71bcf3078
jiyoung25 <115197076+jiyoung25@users.noreply.github.com>
2023-01-11 18:04:45 +0900

    space list와 detail 연결 (#13)

--- TeamProject_SpaceRental/src/main/java/com/team/project/space/controller/SpaceController.java
+++ TeamProject_SpaceRental/src/main/java/com/team/project/space/controller/SpaceController.java
@@ -26,49 +26,9 @@ public class SpaceController {
 	}
 	
 	@GetMapping("/space/detail")
-	public String detail(HttpServletRequest request) {
-		
-		try {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
-		// 현재 날짜 구하기
-        Calendar cal = Calendar.getInstance();
-        String resultDate = sdf.format(cal.getTime());
-        String year = resultDate.substring(0, 4);
-        String month = resultDate.substring(4, 6);
-        String day = resultDate.substring(6, 8);
-        String today = year+"-"+month+"-"+day;
-        System.out.println(today);
-        
-        //내일 날짜 구하기(최소 예약 날짜를 정하기 위해)
-        Calendar cal2 = Calendar.getInstance();
-        Date date = sdf.parse(resultDate);
-        cal2.setTime(date);
-        cal2.add(Calendar.DATE, 1);
-        String resultDate2 = sdf.format(cal2.getTime());
-        String year2 = resultDate2.substring(0, 4);
-        String month2 = resultDate2.substring(4, 6);
-        String day2 = resultDate2.substring(6, 8);
-        String minday = year2+"-"+month2+"-"+day2;
-        System.out.println(minday);
-        
-        //2달 뒤 날짜 구하기(최대 예약 날짜를 정하기 위해)
-        Calendar cal3 = Calendar.getInstance();
-        cal3.setTime(date);
-        cal3.add(Calendar.MONTH, 2);
-        String resultDate3 = sdf.format(cal3.getTime());
-        String year3 = resultDate3.substring(0, 4);
-        String month3 = resultDate3.substring(4, 6);
-        String day3 = resultDate3.substring(6, 8);
-        String maxday = year3+"-"+month3+"-"+day3;
-        System.out.println(maxday);
-        
-        request.setAttribute("today", today);
-        request.setAttribute("minday", minday);
-        request.setAttribute("maxday", maxday);
-        
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public String detail(HttpServletRequest request, int cate_num) {
+		service.getDay(request);
+		request.setAttribute("cate_num", cate_num);
         
 		return("space/detail");
 	}
--- TeamProject_SpaceRental/src/main/webapp/WEB-INF/views/space/list.jsp
+++ TeamProject_SpaceRental/src/main/webapp/WEB-INF/views/space/list.jsp
@@ -21,12 +21,14 @@
 		<div>
 			<c:forEach var="tmp" items="${list }">
 				<div class="space_list">
-					<div> <%-- 이미지 없어서 임시로 넣음 --%>
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
-						  	<path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-						  	<path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/>
-						</svg>
-					</div>
+					<a href="${pageContext.request.contextPath}/space/detail?cate_num=${cate_num }&space_num=${tmp.space_num}">
+						<div> <%-- 이미지 없어서 임시로 넣음 --%>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
+							  	<path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+							  	<path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/>
+							</svg>
+						</div>
+					</a>
 					<div>${tmp.space_name }</div>
 				</div>
 			</c:forEach>
