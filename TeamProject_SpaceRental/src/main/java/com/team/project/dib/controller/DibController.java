3703bb15c30e4504ee236912bfd4721894169fb5
leejiyoung <wldud5117@naver.com>
2023-02-17 16:29:40 +0900

    파일 정리 (들여쓰기, 안쓰는 jsp페이지 삭제)

--- TeamProject_SpaceRental/src/main/java/com/team/project/dib/controller/DibController.java
+++ TeamProject_SpaceRental/src/main/java/com/team/project/dib/controller/DibController.java
@@ -1,8 +1,13 @@
 package com.team.project.dib.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.team.project.dib.dto.DibDto;
 import com.team.project.dib.service.DibService;
@@ -16,17 +21,23 @@ public class DibController {
 	
 	@Auth(role = Role.USER)
 	@RequestMapping("/space/dib_insert")
-	public String insert(DibDto dto, int space_num, int users_num, String users_id) {
+	@ResponseBody
+	public void insert(DibDto dto, int space_num, int users_num, String users_id) {
 		service.dibInsert(dto);
-		
-		return "space/dib_insert";
 	}
 	
 	@Auth(role = Role.USER)
 	@RequestMapping("/space/dib_delete")
-	public String delete(int dibson_num) {
+	@ResponseBody
+	public void delete(int dibson_num) {
 		service.dibDelete(dibson_num);
-		
-		return "space/dib_delete";
+	}
+	
+	@Auth(role = Role.USER)
+	@RequestMapping("/space/dib_list")
+	@ResponseBody
+	public void getDibList(DibDto dto, HttpServletRequest request) {
+		service.dibGetDetailData(dto, request);
+		//HttpServletRequest로 "dib_num"이라는 키에 해당하는 값이 넘어온다.
 	}
 }
