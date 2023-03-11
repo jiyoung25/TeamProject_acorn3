3f8652042433f8ea32090f1720c651e28c3795b8
acorn <acorn@host.docker.internal>
2023-03-11 10:16:10 +0900

    war세팅

--- TeamProject_SpaceRental/src/main/java/com/team/project/TeamProjectSpaceRentalApplication.java
+++ TeamProject_SpaceRental/src/main/java/com/team/project/TeamProjectSpaceRentalApplication.java
@@ -2,12 +2,19 @@ package com.team.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:custom.properties")
-public class TeamProjectSpaceRentalApplication {
-	
+public class TeamProjectSpaceRentalApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TeamProjectSpaceRentalApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TeamProjectSpaceRentalApplication.class, args);
 	}
